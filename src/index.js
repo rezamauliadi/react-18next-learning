d91666fef9a7d272542a3d5dd09821ba87490620
rezamauliadi <reza.mauliadi2@gmail.com>
2020-06-12 20:39:33 +0700

    init i18next

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "antd/dist/antd.css";
+import "./index.css";
+
+import App from "./App";
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import i18nextHttpBackend from "i18next-http-backend";
+
+i18n
+  .use(i18nextHttpBackend)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
